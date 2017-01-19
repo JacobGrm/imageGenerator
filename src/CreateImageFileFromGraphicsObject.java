b6a01ad216a155c2a164cbeafa45dce811f94c96
JacobGrm <JacobGrimberg@yahoo.com>
2017-01-19 10:37:58 -0800

    Update CreateImageFileFromGraphicsObject.java

--- src/CreateImageFileFromGraphicsObject.java
+++ src/CreateImageFileFromGraphicsObject.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+import java.awt.Font;
 
 import javax.imageio.ImageIO;
 
@@ -13,40 +14,47 @@ public class CreateImageFileFromGraphicsObject {
 
         int imageCount = 384;
 
-        int width = 22500;
+        int width = 22000;
         int height = 14000;
-
-        // Constructs a BufferedImage of one of the predefined image types.
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        // Create a graphics which can be used to draw into the buffered image
-        Graphics2D g2d = bufferedImage.createGraphics();
-
-        // fill all the image with white
-        g2d.setColor(Color.white);
-        g2d.fillRect(0, 0, width, height);
-
-        // create a circle with black
-        g2d.setColor(Color.black);
-        g2d.fillOval(0, 0, width, height);
-
-        // create a string with yellow
-        g2d.setColor(Color.RED);
 
         // Save as JPEG
         for(int i=0; i<imageCount; i++){
 
+            // Constructs a BufferedImage of one of the predefined image types.
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            // Create a graphics which can be used to draw into the buffered image
+            Graphics2D g2d = bufferedImage.createGraphics();
+
+            // fill all the image with white
+            g2d.setColor(Color.darkGray);
+            g2d.fillRect(0, 0, width, height);
+
+            // create a circle with black
+            g2d.setColor(Color.black);
+            g2d.fillOval(0, 0, width, height);
+
             String uuid = UUID.randomUUID().toString();
-            System.out.println("UUID = "+ uuid);
-            g2d.drawString("Text: " + uuid, 50, 120);
+            String imageName = "qaImage"+Integer.toString(i)+".jpg";
+            System.out.println(imageName+": "+ uuid);
+
+            Font currentFont = g2d.getFont();
+            Font newFont = currentFont.deriveFont(currentFont.getSize() * 40.4F);
+            g2d.setFont(newFont);
+
+            // create a string with yellow
+            g2d.setColor(Color.white);
+            g2d.drawString("qaImage"+Integer.toString(i)+".jpg: " + uuid, 10, 600);
 
             // Disposes of this graphics context and releases any system resources that it is using.
             g2d.dispose();
 
-            File file = new File("/Users/204071207/QA-16.0-release/images/"+"qaImage"+Integer.toString(i)+".jpg");
+            File file = new File("/Users/204071207/QA-16.0-release/images/"+imageName);
             ImageIO.write(bufferedImage, "jpg", file);
         }
 
     }
 
 }
+
+
